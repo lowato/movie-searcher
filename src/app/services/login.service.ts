42128542e1c780fe8e9be414c478f336946d0376
Pablo Lobato <lobatocriado@gmail.com>
2020-02-01 11:27:08 +0100

    Added set, get and delete methods on login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class LoginService {
 
+  private apiKey = 'api';
+
   constructor() { }
+
+  public setApiKey(value) {
+    sessionStorage.setItem(this.apiKey, JSON.stringify(value));
+  }
+
+  public getApiKey() {
+    return this.apiKey in sessionStorage ? JSON.parse(sessionStorage.getItem(this.apiKey)) : false;
+  }
+
+  public deleteApiKey() {
+    sessionStorage.removeItem(this.apiKey);
+  }
 }
